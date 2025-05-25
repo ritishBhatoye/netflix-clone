ede37a48a0e8816e602d9e7abc99fdf21917d74c
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-25 15:32:16 +0530

    Implement MediaDetailScreen component with detailed media information display, including thumbnail, title, release year, age restriction, duration, description, and action buttons for play and download.

--- components/global/MediaDetailScreen.tsx
+++ components/global/MediaDetailScreen.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-import { View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import Button from "../atoms/Button";
 
 type MediaDetailProps = {
   title: string;
+  thumbnail: string;
   releaseYear: number;
   ageRestriction: string;
   duration: string;
@@ -12,7 +17,57 @@ type MediaDetailProps = {
 };
 
 const MediaDetailScreen = (props: MediaDetailProps) => {
-  return <View className="">MediaDetailScreen</View>;
+  const {
+    title,
+    thumbnail,
+    releaseYear,
+    ageRestriction,
+    duration,
+    description,
+    type,
+  } = props;
+
+  return (
+    <View className="">
+      <Image
+        contentFit="cover"
+        source={{ uri: thumbnail }}
+        style={{ height: "40%" }}
+      />
+      <ScrollView>
+        <Text className="text-base text-white font-bold text-start">
+          {title}
+        </Text>
+        <View className="flex flex-row items-center gap-4">
+          <Text className="text-sm font-normal text-tertiary-100">
+            {releaseYear}
+          </Text>
+          <Text className="text-sm font-normal p-2 px-4 bg-tertiary-400 text-tertiary-100">
+            {ageRestriction}
+          </Text>
+          <Text className="text-sm font-normal p-1 px-2 border-tertiary-100 border text-tertiary-100">
+            HD
+          </Text>
+          <Text className="text-sm font-normal text-tertiary-100">
+            {duration}
+          </Text>
+        </View>
+        <View className="gap-3 my-4 mb-5">
+          <Button variant="isWhite">
+            <Ionicons name="play" size={24} color={"black"} />
+            <Text className="text-lg text-black font-semibold">Play</Text>
+          </Button>
+          <Button variant="secondary">
+            <Ionicons name="download" size={24} color={"white"} />
+            <Text className="text-lg text-black font-semibold">Download</Text>
+          </Button>
+        </View>
+        <Text className="text-sm font-medium text-tertiary-100">
+          {description}
+        </Text>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default MediaDetailScreen;
