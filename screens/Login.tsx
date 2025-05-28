60a442a4c0b27a2a0cca83cea6f4cb0d199495a4
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-28 21:58:30 +0530

    feat : few update in the input atom and /screens/LoginScreen.tsx

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,12 +1,23 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
+import Button from "@/components/atoms/Button";
 import Input from "@/components/atoms/Input";
 
 export default function LoginScreen() {
   return (
     <View className="h-full">
       <Input label={""} placeholder={"Email or phone number"} />
-      <Input label={""} placeholder={"Password"} />
+      <Input
+        // label="Password"
+        placeholder="Password"
+        // value={password}
+        // onChange={setPassword}
+        isPassword={true}
+      />
+
+      <Button label={"Sign In"} variant={"primary"} />
+
+      <Text className="text-white/50 text-center font-light py-4">OR</Text>
     </View>
   );
 }
