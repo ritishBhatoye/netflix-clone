033c2b10f9532ea4f1df2ab9b6f8b6b792adbc90
Ritish Bhatoye <ritishbhatoye@gmail.com>
2025-10-16 22:16:16 +0530

    Refactor VideoPlayer component for improved readability and maintainability. Cleaned up import statements and formatted JSX for better structure.

--- components/atoms/VideoPlayer.tsx
+++ components/atoms/VideoPlayer.tsx
@@ -1,23 +1,30 @@
-import { useEvent } from 'expo';
-import { useVideoPlayer, VideoView } from 'expo-video';
-import { View } from 'react-native';
-import Button from './Button';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+import { useEvent } from "expo";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { View } from "react-native";
+import Button from "./Button";
 
 interface Props {
   videoSource: string;
 }
 
 const VideoPlayer = ({ videoSource }: Props) => {
-  const player = useVideoPlayer(videoSource, player => {
+  const player = useVideoPlayer(videoSource, (player) => {
     player.loop = true;
   });
 
-  const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
+  const { isPlaying } = useEvent(player, "playingChange", {
+    isPlaying: player.playing,
+  });
 
   return (
     <View className="flex-1 items-center justify-center">
-      <VideoView className="w-full h-full" player={player} allowsFullscreen allowsPictureInPicture />
+      <VideoView
+        className="w-full h-full"
+        player={player}
+        allowsFullscreen
+        allowsPictureInPicture
+      />
       <View className="absolute bottom-10">
         <Button
           variant="primary"
@@ -29,7 +36,11 @@ const VideoPlayer = ({ videoSource }: Props) => {
             }
           }}
         >
-          <Ionicons name={isPlaying ? 'pause' : 'play'} color="white" size={20} />
+          <Ionicons
+            name={isPlaying ? "pause" : "play"}
+            color="white"
+            size={20}
+          />
         </Button>
       </View>
     </View>
