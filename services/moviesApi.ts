9ec6068cdbe53242a66dca43ce5158bf894f05b4
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-16 00:42:49 +0530

    add

--- services/moviesApi.ts
+++ services/moviesApi.ts
@@ -10,13 +10,15 @@ export const moviesApi = createApi({
     // Trending Movies (Latest/Featured)
     getTrendingMovies: builder.query<FeaturedMovieProps[], void>({
       query: () => `/trending/movie/day?api_key=${API_KEY}`,
-      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) => response.results,
+      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) =>
+        response.results,
     }),
 
     // Trending TV Shows
     getTrendingTVShows: builder.query<TVShowProps[], void>({
       query: () => `/trending/tv/day?api_key=${API_KEY}`,
-      transformResponse: (response: TMDBResponse<TVShowProps>) => response.results,
+      transformResponse: (response: TMDBResponse<TVShowProps>) =>
+        response.results,
     }),
 
     // Top 10 Today (Popular Movies)
@@ -35,50 +37,62 @@ export const moviesApi = createApi({
 
     // Netflix Originals (Using Netflix as production company - ID: 213)
     getNetflixOriginals: builder.query<FeaturedMovieProps[], void>({
-      query: () => `/discover/movie?api_key=${API_KEY}&with_companies=213&sort_by=popularity.desc`,
-      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) => response.results,
+      query: () =>
+        `/discover/movie?api_key=${API_KEY}&with_companies=213&sort_by=popularity.desc`,
+      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) =>
+        response.results,
     }),
 
     // Netflix Original TV Shows
     getNetflixOriginalShows: builder.query<TVShowProps[], void>({
-      query: () => `/discover/tv?api_key=${API_KEY}&with_networks=213&sort_by=popularity.desc`,
-      transformResponse: (response: TMDBResponse<TVShowProps>) => response.results,
+      query: () =>
+        `/discover/tv?api_key=${API_KEY}&with_networks=213&sort_by=popularity.desc`,
+      transformResponse: (response: TMDBResponse<TVShowProps>) =>
+        response.results,
     }),
 
     // Top Rated Movies
     getTopRatedMovies: builder.query<FeaturedMovieProps[], void>({
       query: () => `/movie/top_rated?api_key=${API_KEY}&page=1`,
-      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) => response.results,
+      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) =>
+        response.results,
     }),
 
     // Top Rated TV Shows
     getTopRatedTVShows: builder.query<TVShowProps[], void>({
       query: () => `/tv/top_rated?api_key=${API_KEY}&page=1`,
-      transformResponse: (response: TMDBResponse<TVShowProps>) => response.results,
+      transformResponse: (response: TMDBResponse<TVShowProps>) =>
+        response.results,
     }),
 
     // Upcoming Movies
     getUpcomingMovies: builder.query<FeaturedMovieProps[], void>({
       query: () => `/movie/upcoming?api_key=${API_KEY}&page=1`,
-      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) => response.results,
+      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) =>
+        response.results,
     }),
 
     // Now Playing Movies
     getNowPlayingMovies: builder.query<FeaturedMovieProps[], void>({
       query: () => `/movie/now_playing?api_key=${API_KEY}&page=1`,
-      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) => response.results,
+      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) =>
+        response.results,
     }),
 
     // Movies by Genre
     getMoviesByGenre: builder.query<FeaturedMovieProps[], number>({
-      query: (genreId) => `/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&sort_by=popularity.desc`,
-      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) => response.results,
+      query: (genreId) =>
+        `/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&sort_by=popularity.desc`,
+      transformResponse: (response: TMDBResponse<FeaturedMovieProps>) =>
+        response.results,
     }),
 
     // TV Shows by Genre
     getTVShowsByGenre: builder.query<TVShowProps[], number>({
-      query: (genreId) => `/discover/tv?api_key=${API_KEY}&with_genres=${genreId}&sort_by=popularity.desc`,
-      transformResponse: (response: TMDBResponse<TVShowProps>) => response.results,
+      query: (genreId) =>
+        `/discover/tv?api_key=${API_KEY}&with_genres=${genreId}&sort_by=popularity.desc`,
+      transformResponse: (response: TMDBResponse<TVShowProps>) =>
+        response.results,
     }),
 
     // Get Movie Genres
